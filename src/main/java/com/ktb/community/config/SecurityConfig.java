83418ce8ae53e2d9329e05ddb24f3363d410cc5d
JunBeom Bae <baejun10@korea.ac.kr>
2025-10-20 09:41:00 +0900

    fix: /error 경로는 항상 허용
    - 허용하지 않으면 비인증 사용자는 오류 확인 불가

--- src/main/java/com/ktb/community/config/SecurityConfig.java
+++ src/main/java/com/ktb/community/config/SecurityConfig.java
@@ -2,6 +2,8 @@ package com.ktb.community.config;
 
 import com.ktb.community.user.UserSecurityService;
 
+import jakarta.servlet.DispatcherType;
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -12,6 +14,7 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.RequestCacheConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
@@ -29,6 +32,7 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
+
                 .cors(cors -> cors.configurationSource(corsConfigurationSource))
                 .csrf(csrf -> csrf.disable())
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.IF_REQUIRED))
@@ -37,15 +41,23 @@ public class SecurityConfig {
                     securityContext.requireExplicitSave(false); // 로그인 세션 자동 저장 해야 로그인 세션 유지 됨.
                 })
                 .authorizeHttpRequests(auth -> auth
+                        .dispatcherTypeMatchers(DispatcherType.ERROR, DispatcherType.FORWARD).permitAll()
                         .requestMatchers(HttpMethod.GET, "/swagger-ui.html","/swagger-ui/**","/v3/api-docs/**").permitAll()
                         .requestMatchers(HttpMethod.POST, "/api/auth/login").permitAll()
                         .requestMatchers(HttpMethod.POST, "/api/users").permitAll()
                         .requestMatchers(HttpMethod.GET, "/api/users/check-email").permitAll()
                         .requestMatchers(HttpMethod.GET, "/api/posts/**").permitAll()
                         .requestMatchers(HttpMethod.GET, "/", "/static/**", "/templates/**").permitAll()
+                        .requestMatchers(HttpMethod.OPTIONS, "/**").permitAll() // 프리플라이트 허용
                         .anyRequest().authenticated()
                 )
-                .logout(logout -> logout.logoutUrl("/api/auth/logout").deleteCookies("JSESSIONID"))
+                .logout(logout -> logout
+                        .logoutUrl("/api/auth/logout").deleteCookies("JSESSIONID")
+                        .logoutSuccessHandler((request, response, authentication) -> {
+                            SecurityContextHolder.clearContext();
+                            response.setStatus(HttpServletResponse.SC_OK);
+                        })
+                )
                 .requestCache(RequestCacheConfigurer::disable);
                 // 인증 오류 시 401 상태코드 리턴
 //                .exceptionHandling(ex -> ex
